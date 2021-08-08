38c89eb0f79f47dbe9591007a2b7872a19631b0f
aniketmund21 <aniiketmund53@gmail.com>
2021-08-08 14:52:22 +0530

    Added Tab Button Functionality

--- index.js
+++ index.js
@@ -1,39 +1,25 @@
 let myLeads = []
+let oldLeads =  []
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn")
 let ulEl = document.getElementById("ul-el")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads"))
 let deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
 
 if(leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
-
-deleteBtn.addEventListener("dblclick", function(){
-    localStorage.clear()
-    myLeads = []
-    renderLeads()
-})
-
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    renderLeads()
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-})
-
-
-
-function renderLeads(){
+function render(leads){
     let listItems = ""
-    for(let i=0 ; i<myLeads.length ; i++){
+    for(let i=0 ; i<leads.length ; i++){
         listItems += `
         <li>
-        <a target ='_blank' href='${ myLeads[i]}'>
-        ${ myLeads[i]}
+        <a target ='_blank' href='${ leads[i]}'>
+        ${ leads[i]}
         </a>
         </li>`
         
@@ -41,6 +27,37 @@ function renderLeads(){
 
     ulEl.innerHTML = listItems
 }
+
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    render(myLeads)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+})
+
+tabBtn.addEventListener("click", function(){
+    
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+        
+   
+    });
+
+    
+
+})
+
+
 
 //Peaky Blinders 
 
